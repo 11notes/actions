6a05844f23b44209f2b38bd42b93cb773fab1f19
ElevenNotes <github@11notes.ch>
2026-08-19 20:32:14 +0200

    test: trap

--- update/index.js
+++ update/index.js
@@ -2,23 +2,11 @@ import getEleven from './src/Eleven.mjs';
 import Action from './src/Update.mjs';
 const Eleven = getEleven();
 
-process
-  .on('unhandledRejection', (reason, p) => {
-    Eleven.warning('unhandledRejection', reason, p);
-  })
-  .on('uncaughtException', e => {
-    Eleven.warning('uncaughtException', e);
-  });
-
 (async()=>{
-  try{
-    const action = new Action();
-    if(await action.verify()){
-      await action.run();
-    }else{
-      Eleven.warning("action.verify() failed! check your input variables")
-    }
-  }catch(e){
-    Eleven.error(e);
+  const action = new Action();
+  if(await action.verify()){
+    await action.run();
+  }else{
+    Eleven.warning("action.verify() failed! check your input variables")
   }
 })();
--- update/test.js
+++ update/test.js
@@ -1,22 +1,17 @@
 import { existsSync } from 'node:fs';
 import { resolve } from 'node:path';
 
-const bundlePath = resolve('./dist/index.js');
-if(!existsSync(bundlePath)){
-  console.error('build failed, no index.js found!');
-  process.exit(1);
-}
+(async()=>{
+  const bundlePath = resolve('./dist/index.js');
+  if(!existsSync(bundlePath)){
+    console.error('build failed, no index.js found!');
+    process.exit(1);
+  }
 
-process.env.DEBUG = true;
-process.env.INPUT_LATEST = '1.0.0';
-process.env.INPUT_VERBOSE = 'true';
-process.env.GITHUB_WORKSPACE = process.cwd();
+  process.env.DEBUG = true;
+  process.env.INPUT_LATEST = '1.0.0';
+  process.env.INPUT_VERBOSE = 'true';
+  process.env.GITHUB_WORKSPACE = process.cwd();
 
-console.log('testing github action ...');
-
-try{
   await import(bundlePath);
-}catch(err){
-  console.error('❌ execution error in action:', err);
-  process.exit(1);
-}
+})();

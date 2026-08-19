1e2c0e7598620f8f963323d685c42f071a5d6b7a
ElevenNotes <github@11notes.ch>
2026-08-19 20:55:29 +0200

    feat: add test.ok.js

---
+++ update/test.ok.js
@@ -0,0 +1,17 @@
+import { existsSync } from 'node:fs';
+import { resolve } from 'node:path';
+
+(async()=>{
+  const bundlePath = resolve('./dist/index.js');
+  if(!existsSync(bundlePath)){
+    console.error('build failed, no index.js found!');
+    process.exit(1);
+  }
+
+  process.env.DEBUG = true;
+  process.env.INPUT_LATEST = '1.29.0';
+  process.env.INPUT_VERBOSE = 'true';
+  process.env.GITHUB_WORKSPACE = process.cwd();
+
+  await import(bundlePath);
+})();
